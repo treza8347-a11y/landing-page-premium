532dec9ead6959a47cf312614ed89eed2a4cdd80
treza8347-a11y <treza8347@gmail.com>
2026-03-03 13:16:17 +0100

    Add POST handler for webhook in lead.js

--- api/lead.js
+++ api/lead.js
@@ -1,1 +1,24 @@
 
+export default async function handler(req, res) {
+
+if (req.method !== "POST") {
+return res.status(405).send("Method not allowed");
+}
+
+try {
+
+await fetch("COLLE_ICI_TON_WEBHOOK_ZAPIER", {
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify(req.body)
+});
+
+return res.status(200).send("OK");
+
+} catch (error) {
+return res.status(500).send("Server Error");
+}
+
+}
